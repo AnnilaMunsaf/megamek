015c7526ef95fdee7424b42d64136fd5b52421fb
Annila Munsaf <annila.munsaf@synesthesia.it>
2023-05-20 15:35:05 +0200

    unit test added for server

--- megamek/unittests/megamek/server/ServerTest.java
+++ megamek/unittests/megamek/server/ServerTest.java
@@ -15,46 +15,40 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Vector;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(JUnit4.class)
 public class ServerTest {
 
     IGame game;
-    Player player1;
     Player player2;
 
 
     @Before
     public void setUp() {
-        game = Mockito.mock(Game.class);
-        player1 = new Player(1, "Player1");
+        game = mock(Game.class);
         player2 = new Player(2, "Player2");
-        game.addPlayer(1, player1);
-        game.addPlayer(2, player2);
-
-
     }
 
     @Test
     public void testForceVictoryForTeam() throws IOException {
 
         // Create a mock player
-        IPlayer victor = Mockito.mock(IPlayer.class);
+        IPlayer victor = mock(IPlayer.class);
         Mockito.doNothing().when(victor).setAdmitsDefeat(false);
 
         // Create a mock player vector
         Vector<IPlayer> playersVector = new Vector<>();
         for (int i = 0; i < 2; i++) {
-            IPlayer player = Mockito.mock(IPlayer.class);
+            IPlayer player = mock(IPlayer.class);
             playersVector.add(player);
         }
         when(game.getPlayersVector()).thenReturn(playersVector);
 
-        Mockito.when(game.getEntities()).thenReturn(Mockito.mock(Iterator.class));
-        Mockito.when(game.getPlayers()).thenReturn(Mockito.mock(Enumeration.class));
-        Mockito.when(game.getAttacks()).thenReturn(Mockito.mock(Enumeration.class));
+        Mockito.when(game.getEntities()).thenReturn(mock(Iterator.class));
+        Mockito.when(game.getPlayers()).thenReturn(mock(Enumeration.class));
+        Mockito.when(game.getAttacks()).thenReturn(mock(Enumeration.class));
 
         Mockito.when(victor.getTeam()).thenReturn(2);
         Mockito.when(victor.getId()).thenReturn(1);
@@ -76,29 +70,31 @@ public class ServerTest {
         for (IPlayer player : playersVector) {
             verify(player).setAdmitsDefeat(false);
         }
+
+        server.die();
+
     }
 
     @Test
     public void testForceVictoryForPlayer() throws IOException {
 
         // Create a mock player
-        IPlayer victor = Mockito.mock(IPlayer.class);
+        IPlayer victor = mock(IPlayer.class);
         when(victor.getId()).thenReturn(2);
-        Mockito.doNothing().when(victor).setAdmitsDefeat(false);
 
         // Create a mock player vector
         Vector<IPlayer> playersVector = new Vector<>();
         for (int i = 0; i < 2; i++) {
-            IPlayer player = Mockito.mock(IPlayer.class);
+            IPlayer player = mock(IPlayer.class);
             playersVector.add(player);
         }
         when(game.getPlayersVector()).thenReturn(playersVector);
 
-        Mockito.when(game.getEntities()).thenReturn(Mockito.mock(Iterator.class));
-        Mockito.when(game.getPlayers()).thenReturn(Mockito.mock(Enumeration.class));
-        Mockito.when(game.getAttacks()).thenReturn(Mockito.mock(Enumeration.class));
+        Mockito.when(game.getEntities()).thenReturn(mock(Iterator.class));
+        Mockito.when(game.getPlayers()).thenReturn(mock(Enumeration.class));
+        Mockito.when(game.getAttacks()).thenReturn(mock(Enumeration.class));
 
-        Server server = new Server("Server 1", 5);
+        Server server = new Server("Server 2", 5);
         server.setGame(game);
 
         server.forceVictory(victor);
@@ -116,6 +112,28 @@ public class ServerTest {
         for (IPlayer player : playersVector) {
             verify(player).setAdmitsDefeat(false);
         }
+
+        server.die();
+
+    }
+
+    @Test
+    public void testForceVictory() throws IOException {
+
+        // Create the server and set the game
+        Server server = new Server("Server 3", 5);
+        Game game = new Game();
+
+        server.setGame(game);
+
+        // Invoke the method being tested
+        server.forceVictory(player2);
+
+        // Assert the expected outcome
+        assertEquals(2, server.getGame().getVictoryPlayerId());
+
+        server.die();
+
     }
 
 
